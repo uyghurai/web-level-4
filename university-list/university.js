a1af093f747b7acc5667dbd9a4056d8571d5a5f9
uyghurai <admin@uyghur.ai>
2022-12-06 20:55:43 -0500

    refactored university.js
    refactoring university.js

--- university-list/university.js
+++ university-list/university.js
@@ -1,13 +1,34 @@
 $(document).ready(function() {
-    //$("universityList").html("");
-    $.get("http://universities.hipolabs.com/search?country=Canada", function(data, status) {
-        if (status == "success" && data != null && data != undefined) {
-            for (let index = 0; index < data.length; index++) {
-                $("#universityList").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="list-group-item list-group-item-action">${data[index].name}</a>`);
 
-            }
-        } else {
-            $("#universityList").append("Can't load the list from url.");
-        }
+    setButtonText();
+    generateUniversitiesList(getSelectedValue());
+
+    $("#target").change(function() {
+        setButtonText();
+        generateUniversitiesList(getSelectedValue());
+
     });
+
+    function getSelectedValue() {
+        return $('#target').find(":selected").val();
+    }
+
+    function setButtonText() {
+        $("#collapseButton").html("Universities in " + $('#target').find(":selected").text());
+    }
+
+    function generateUniversitiesList(country) {
+        var urlLink = "http://universities.hipolabs.com/search?country=" + country;
+        $.get(urlLink, function(data, status) {
+            if (status == "success" && data != null && data != undefined) {
+                $("#universityList").html('');
+                for (let index = 0; index < data.length; index++) {
+                    $("#universityList").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="list-group-item list-group-item-action">${data[index].name}</a>`);
+
+                }
+            } else {
+                $("#universityList").append("Can't load the list from url.");
+            }
+        });
+    }
 });
